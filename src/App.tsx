7a0b9c9cd6ce082b605e30012e64b8c86ee21d3d
Shreyanka Padennavar <shreyanka1105@gmail.com>
2026-02-15 21:59:15 +0530

    Structure Updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Skills from "./pages/Skills";
-import Resume from "./pages/Resume";
-import Jobs from "./pages/Jobs";
+// import Resume from "./pages/Resume";
+// import Jobs from "./pages/Jobs";
 import Projects from "./pages/Projects";
-import Education from "./pages/Education";
+//import Education from "./pages/Education";
 import "./styles.css";
 
 export default function App() {
@@ -15,10 +15,10 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/skills" element={<Skills />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/jobs" element={<Jobs />} />
+        {/* <Route path="/resume" element={<Resume />} /> */}
+        {/* <Route path="/jobs" element={<Jobs />} /> */}
         <Route path="/projects" element={<Projects />} />
-        <Route path="/education" element={<Education />} />
+        {/* <Route path="/education" element={<Education />} /> */}
       </Routes>
     </BrowserRouter>
   );
